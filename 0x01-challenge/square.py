de96309fee4ca03a004597441dd0c570d2e11e3a
= <=>
2024-07-15 00:56:02 +0100

    test

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -5,9 +5,9 @@ class Square():
     width = 0
     height = 0
 
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
     def area_of_my_square(self):
         """ Area of the square """
